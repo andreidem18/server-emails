805d1e3853f355581615bb72caad7377c0923bdc
Andrés Mendoza <andres.david.mm@hotmail.com>
2021-05-02 11:39:59 -0500

    testing

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.get("/send-email", (req, res) => {
     res.send("Emails route");
 })
 
-app.post("/send-email", (req, res) => {
-    const {name, email, phone, message} = req.body;
-    enviarCorreo(name, email, phone, message);
-    res.json({
-        message: "the email was sent"
-    });
-});
+// app.post("/send-email", (req, res) => {
+//     const {name, email, phone, message} = req.body;
+//     enviarCorreo(name, email, phone, message);
+//     res.json({
+//         message: "the email was sent"
+//     });
+// });
 
 app.listen(8000, () => {
     console.log("Servidor iniciado en el puerto 8000");
