862f0abaf4ab837e35771ce5f2fe68d0644a96a6
Andrés Mendoza <andres.david.mm@hotmail.com>
2021-05-02 11:52:09 -0500

    deleting comments

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.get("/send-email", (req, res) => {
     res.send("Emails route");
 })
 
-// app.post("/send-email", (req, res) => {
-//     const {name, email, phone, message} = req.body;
-//     enviarCorreo(name, email, phone, message);
-//     res.json({
-//         message: "the email was sent"
-//     });
-// });
+app.post("/send-email", (req, res) => {
+    const {name, email, phone, message} = req.body;
+    enviarCorreo(name, email, phone, message);
+    res.json({
+        message: "the email was sent"
+    });
+});
 
 app.listen(PORT, () => {
     console.log("Server listening on", PORT);
