4076ba5a29e6bd93fba5e575433072910a035fce
Andrés Mendoza <andres.david.mm@hotmail.com>
2021-05-08 08:31:23 -0500

    cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
-app.use(cors({origin: 'https://andresmendoza.netlify.app/'}));
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Home rute");
